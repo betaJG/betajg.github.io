ad5d1c9774f2766fff0a62e51eefbfb3797022ab
Beta Gurdi <112025349+betaJG@users.noreply.github.com>
2025-11-14 21:50:31 -0600

    Add files via upload

---
+++ q1get.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        Welcome, <?php echo $_GET["Name"]; ?>.<br/>
+        
+
+        
+        You have reached the login php page
+        
+
+        <!-- Your password is <?php echo $_GET["password"]; ?> -->
+
+        Your hidden role was: <?php echo $_GET["role"]; ?>
+    </body>
+</html>
